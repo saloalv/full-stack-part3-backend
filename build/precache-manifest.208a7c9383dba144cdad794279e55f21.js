b8efad00d248b7192302c3fc9973192cf3db1026
Alvar Salokangas <saloalv@live.com>
2019-06-17 22:38:36 +0300

    added frontend

---
+++ build/precache-manifest.208a7c9383dba144cdad794279e55f21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917d799b019c10e09d8a1767707971ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ff666b40a4178d2c43e",
+    "url": "/static/css/main.d0932e76.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "9ff666b40a4178d2c43e",
+    "url": "/static/js/main.92604428.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
